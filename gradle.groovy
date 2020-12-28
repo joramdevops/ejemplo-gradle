a43c3d3e4041bf02892cbdd447242671605e59b1
Man <73675618+joramdevops@users.noreply.github.com>
2020-12-28 01:25:01 -0300

    Update gradle.groovy

--- gradle.groovy
+++ gradle.groovy
@@ -6,9 +6,30 @@
 
 def call(){
   
-  stage(){
-    //Escribir directamente el código del stage, sin agregarle otra clausula de Jenkins.
-  }
+
+					stage('Build & Test') {
+						sh "./gradlew clean build" 
+					}
+
+					stage('Sonar'){
+						def scannerHome = tool 'sonar-scanner';
+						 withSonarQubeEnv(installationName: 'sonar-server') {
+							sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
+						}
+					}
+
+					stage('Run'){
+						sh "nohup gradlew bootRun &"
+						sleep 20
+					}
+
+					stage('Check app'){
+						sh 'curl -X GET http://localhost:8081/rest/mscovid/test?msg=testing'
+					}
+
+					stage('Nexus'){
+						nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'build/libs/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
+					}
 
 }
 
