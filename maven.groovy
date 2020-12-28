2d46c273bc649dfa6268bdbbff884f647d52e6d0
Man <73675618+joramdevops@users.noreply.github.com>
2020-12-28 01:55:05 -0300

    Update maven.groovy

--- maven.groovy
+++ maven.groovy
@@ -1,30 +1,33 @@
-def call(){
+def call()  {
   
-stage('Compile Code') {
-            steps {
-                sh './mvnw clean compile -e'
-            }
-        }
-        stage('Test Code') {
-            steps {              
-                sh './mvnw clean test -e'
-            }
-        }
-        stage('Jar ') {
-            steps {               
-                sh './mvnw clean package -e'
-            }
-        }
-        stage('SonarQube') {
-           steps {
-             withSonarQubeEnv(installationName: 'sonar-server') { 
-             sh './mvnw org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar'
-             }
-          }
-        }
-        stage('Upload Nexus') {
-            steps {               
-                nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: '/Users/joram/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
-            }
-        }  
+    stage('Compile') {
+         
+          sh './mvnw clean compile -e'
+          
+         }
+        stage('Test') {
+         
+          sh './mvnw clean test -e'
+        
+        } 
+      stage('Jar') {
+       
+         sh './mvnw clean package -e'
+      }
+      
+      stage('sonar') {
+        
+          withSonarQubeEnv(installationName: 'Sonar') {    
+           sh './mvnw org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar' 
+        
+       }
+      }  
+
+      stage('uploadNexus') {
+         
+          nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'build/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
+         
+       }   
 }
+
+return this;
