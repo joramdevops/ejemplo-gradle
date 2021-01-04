72d4d5b89d6f1a8ddc76493c12981b8a6469b8b4
Man <73675618+joramdevops@users.noreply.github.com>
2021-01-04 00:19:51 -0300

    Update maven.groovy

--- maven.groovy
+++ maven.groovy
@@ -7,25 +7,25 @@ def call()  {
          }
         stage('Test') {
          
-          sh 'mvn clean test -e'
+          sh './mvnw clean test -e'
         
         } 
       stage('Jar') {
        
-         sh 'mvn clean package -e'
+         sh './mvnw clean package -e'
       }
       
-      stage('sonar') {
+      stage('Sonar') {
         
-          withSonarQubeEnv(installationName: 'Sonar') {    
-           sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar' 
+          withSonarQubeEnv(installationName: 'sonar-server') {    
+           sh './mvnw org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar' 
         
        }
       }  
 
       stage('uploadNexus') {
          
-          nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'build/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
+          nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'build/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.2']]]
          
        }   
 }
