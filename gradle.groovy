b44315ad137d9d30b53fe8d98988110ebea0219e
Man <73675618+joramdevops@users.noreply.github.com>
2021-01-04 01:18:32 -0300

    Update gradle.groovy

--- gradle.groovy
+++ gradle.groovy
@@ -28,7 +28,7 @@ def call(){
 	}
 
 	stage('Nexus'){
-		nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'build/libs/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
+		nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'GradlePackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'build/libs/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.4']]]
 	}
 }
 
