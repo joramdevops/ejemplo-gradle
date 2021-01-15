d38200c60c195b30693759340f712675b19c2f5d
Man <73675618+joramdevops@users.noreply.github.com>
2021-01-15 19:14:17 -0300

    Update gradle.groovy

--- gradle.groovy
+++ gradle.groovy
@@ -26,10 +26,17 @@ def call(){
 	stage('Rest'){
 		sh "curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
 	}
-
+	
 	stage('Nexus'){
-		nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'build/libs/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.4']]]
+		nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'build/libs/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.11']]]
 	}
+	
+	stage('gitTagMaster') {
+            when { tag "release-*" }
+            steps {
+                echo "Implementar solo si la confirmacion esta etiquetada"
+            }
+        }
 }
 
 return this;
