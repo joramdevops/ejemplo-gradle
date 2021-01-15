41a29b7797cac46527dcc267b38d089d662c9e21
Man <73675618+joramdevops@users.noreply.github.com>
2021-01-15 20:06:45 -0300

    Update gradle.groovy

--- gradle.groovy
+++ gradle.groovy
@@ -31,13 +31,18 @@ def call(){
 		nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'build/libs/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: 'release-0.0.11']]]
 	}
 	
-	stage('gitTagMaster') {
-	    figlet 'Tag'
-            when { tag "release-*" }
-            steps {
-                echo "Implementar solo si la confirmacion esta etiquetada"
-            }
-        }
+	stage('gitTagMaster'){
+    		echo "Tagging master with version: ${version}"
+    		fetchMasterFromOrigin()
+    		sshagent([sshKey]) {
+        		int status = sh returnStatus: true, script: "git checkout master && git tag ${version} && git push --tag"
+        		if (status != 0) {
+            		echo "Failed to tag master branch with tag ${version}."
+        		}
+    		}
+	}
+	
+	
 }
 
 return this;
